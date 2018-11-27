3a66f8dd18bb32553f8f597bfab3e392a6c701da
Naphorn <31033972+Naphorn@users.noreply.github.com>
2018-11-27 16:33:08 +0700

    edited

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -81,7 +81,7 @@
 
             <div class="content">
                 <div class="title m-b-md">
-                    Managent Air
+                    Management Air
                 </div>
             </div>
         </div>

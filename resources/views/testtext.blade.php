de1492adac183170898cc42d64b1a6a60ca5aa27
Naphorn Chokpokasombat <31033972+Naphorn@users.noreply.github.com>
2019-02-12 16:56:06 +0700

    Update testtext.blade.php

--- resources/views/testtext.blade.php
+++ resources/views/testtext.blade.php
@@ -36,22 +36,140 @@
             border-bottom: solid 0px #00ccff;
             border-right: solid 0px #00ccff;
         }
+        #line5{
+            border-left: solid 0px #00ccff;
+            border-top: solid 0px #00ccff;
+            border-bottom: solid 0px #00ccff;
+            border-right: solid 0px #00ccff;
+        }
+        #line6{
+            border-left: solid 0px #00ccff;
+            border-top: solid 0px #00ccff;
+            border-bottom: solid 0px #00ccff;
+            border-right: solid 0px #00ccff;
+        }
+        #line7{
+            border-left: solid 0px #00ccff;
+            border-top: solid 0px #00ccff;
+            border-bottom: solid 0px #00ccff;
+            border-right: solid 0px #00ccff;
+        }
+        #line8{
+            border-left: solid 0px #00ccff;
+            border-top: solid 0px #00ccff;
+            border-bottom: solid 0px #00ccff;
+            border-right: solid 0px #00ccff;
+        }
+        #line9{
+            border-left: solid 0px #00ccff;
+            border-top: solid 0px #00ccff;
+            border-bottom: solid 0px #00ccff;
+            border-right: solid 0px #00ccff;
+        }
+        #line10{
+            border-left: solid 0px #00ccff;
+            border-top: solid 0px #00ccff;
+            border-bottom: solid 0px #00ccff;
+            border-right: solid 0px #00ccff;
+        }
+        #line11{
+            border-left: solid 0px #00ccff;
+            border-top: solid 0px #00ccff;
+            border-bottom: solid 0px #00ccff;
+            border-right: solid 0px #00ccff;
+        }
+        #line12{
+            border-left: solid 0px #00ccff;
+            border-top: solid 0px #00ccff;
+            border-bottom: solid 0px #00ccff;
+            border-right: solid 0px #00ccff;
+        }
+        #line13{
+            border-left: solid 0px #00ccff;
+            border-top: solid 0px #00ccff;
+            border-bottom: solid 0px #00ccff;
+            border-right: solid 0px #00ccff;
+        }
+        #line14{
+            border-left: solid 0px #00ccff;
+            border-top: solid 0px #00ccff;
+            border-bottom: solid 0px #00ccff;
+            border-right: solid 0px #00ccff;
+        }
+        #line15{
+            border-left: solid 0px #00ccff;
+            border-top: solid 0px #00ccff;
+            border-bottom: solid 0px #00ccff;
+            border-right: solid 0px #00ccff;
+        }
+        #line16{
+            border-left: solid 0px #00ccff;
+            border-top: solid 0px #00ccff;
+            border-bottom: solid 0px #00ccff;
+            border-right: solid 0px #00ccff;
+        }
 /* ------------------------------------------------------------------ */
         #line1.active{
             border-left: solid 5px #00ccff;
-            height: 50px;
         }
         #line2.active{
             border-top: solid 5px #00ccff;
-            width: 50px;
         }
         #line3.active{
-            border-bottom: solid 5px #00ccff;
-            width: 50px;
+            border-left: solid 5px #00ccff;
+            border-top: solid 5px #00ccff;
         }
         #line4.active{
             border-right: solid 5px #00ccff;
-            height: 50px;
+        }
+        #line5.active{
+            border-left: solid 5px #00ccff;
+            border-right: solid 5px #00ccff;
+        }
+        #line6.active{
+            border-top: solid 5px #00ccff;
+            border-right: solid 5px #00ccff;
+        }
+        #line7.active{
+            border-left: solid 5px #00ccff;
+            border-top: solid 5px #00ccff;
+            border-right: solid 5px #00ccff;
+        }
+        #line8.active{
+            border-bottom: solid 5px #00ccff;
+        }
+        #line9.active{
+            border-left: solid 5px #00ccff;
+            border-bottom: solid 5px #00ccff;
+        }
+        #line10.active{
+            border-top: solid 5px #00ccff;
+            border-bottom: solid 5px #00ccff;
+        }
+        #line11.active{
+            border-left: solid 5px #00ccff;
+            border-top: solid 5px #00ccff;
+            border-bottom: solid 5px #00ccff;
+        }
+        #line12.active{
+            border-bottom: solid 5px #00ccff;
+            border-right: solid 5px #00ccff;
+        }
+        #line13.active{
+            border-left: solid 5px #00ccff;
+            border-bottom: solid 5px #00ccff;
+            border-right: solid 5px #00ccff;
+        }
+        #line14.active{
+            border-top: solid 5px #00ccff;
+            border-bottom: solid 5px #00ccff;
+            border-right: solid 5px #00ccff;
+        }
+        #line15.active{
+            border-left: solid 5px #00ccff;
+            border-top: solid 5px #00ccff;
+            border-bottom: solid 5px #00ccff;
+            border-right: solid 5px #00ccff;
         }
 /* ------------------------------------------------------------------ */
     </style>
@@ -59,44 +177,14 @@
 </head>
 <body>
 
-    <canvas id="myCanvas"></canvas>
     <script>
-
-        var canvas = document.getElementById('myCanvas');
-        var context = canvas.getContext('2d');
-        // ----- ซ้าย -------------------
-        context.beginPath();
-        context.strokeStyle = "#3984dd";
-        context.lineWidth = 2;
-        context.moveTo(50, 50);
-        context.lineTo(50, 100);
-        context.stroke();
-
-        // ----- ขวา -------------------
-        context.beginPath();
-        context.strokeStyle = "#FF0000";
-        context.lineWidth = 2;
-        context.moveTo(100, 50);
-        context.lineTo(100, 100);
-        context.stroke();
-
-        // ----- บน --------------------
-        context.beginPath();
-        context.strokeStyle = "#FFFF00";
-        context.lineWidth = 2;
-        context.moveTo(100, 50);
-        context.lineTo(50, 50);
-        context.stroke();
-
-        // ----- ล่าง -------------------
-        context.beginPath();
-        context.strokeStyle = "#33FF00";
-        context.lineWidth = 2;
-        context.moveTo(100, 100);
-        context.lineTo(50, 100);
-        context.stroke();
-
         function toggleLine1() {
+            var a = 0;
+            var b = 0;
+            var c = 0;
+            var d = 0;
+
+            if ()
             document.getElementById("line1").classList.toggle('active');
         }
         function toggleLine2() {
@@ -108,10 +196,42 @@
         function toggleLine4() {
             document.getElementById("line4").classList.toggle('active');
         }
-
+        // function toggleLine5() {
+        //     document.getElementById("line4").classList.toggle('active');
+        // }
+        // function toggleLine6() {
+        //     document.getElementById("line5").classList.toggle('active');
+        // }
+        // function toggleLine7() {
+        //     document.getElementById("line6").classList.toggle('active');
+        // }
+        // function toggleLine8() {
+        //     document.getElementById("line7").classList.toggle('active');
+        // }
+        // function toggleLine9() {
+        //     document.getElementById("line8").classList.toggle('active');
+        // }
+        // function toggleLine10() {
+        //     document.getElementById("line9").classList.toggle('active');
+        // }
+        // function toggleLine11() {
+        //     document.getElementById("line10").classList.toggle('active');
+        // }
+        // function toggleLine12() {
+        //     document.getElementById("line11").classList.toggle('active');
+        // }
+        // function toggleLine13() {
+        //     document.getElementById("line12").classList.toggle('active');
+        // }
+        // function toggleLine14() {
+        //     document.getElementById("line13").classList.toggle('active');
+        // }
+        // function toggleLine15() {
+        //     document.getElementById("line14").classList.toggle('active');
+        // }
     </script>
 
-    <table width=50% border="5" cellpadding="50" cellspacing="0">
+    <table width=25% border="5" cellpadding="50" cellspacing="0">
 
         <th id="line1"></th>
         <th id="line2"></th>
@@ -120,10 +240,11 @@
 
     </table>
 
-    <button type="button" class="toggle-btn" onclick="toggleLine1()">line1</button>
-    <button type="button" class="toggle-btn" onclick="toggleLine2()">line2</button>
-    <button type="button" class="toggle-btn" onclick="toggleLine3()">line3</button>
-    <button type="button" class="toggle-btn" onclick="toggleLine4()">line4</button>
+    <button type="button" class="toggle-btn" onclick="toggleLine1()">1</button>
+    <button type="button" class="toggle-btn" onclick="toggleLine2()">2</button>
+    <button type="button" class="toggle-btn" onclick="toggleLine3()">4</button>
+    <button type="button" class="toggle-btn" onclick="toggleLine4()">8</button>
 
 </body>
 </html>
+

fb8e11be26ef1d4ac5933eb981f5182fbd2fa305
Woraprad <31096955+Woraprad@users.noreply.github.com>
2019-03-03 22:51:18 +0700

    หน้า welcome ใหม่

---
+++ public/js/welcome1.js
@@ -0,0 +1,19 @@
+var str = "ระบบริหารจัดการเครื่องปรับอากาศ  ",
+    i = 0,
+    isTag,
+    text;
+
+(function type() {
+    text = str.slice(0, ++i);
+    if (text === str) return;
+    
+    document.getElementById('typewriter').innerHTML = text;
+
+    var char = text.slice(-1);
+    if( char === '<' ) isTag = true;
+    if( char === '>' ) isTag = false;
+
+    if (isTag) return type();
+    setTimeout(type, 150);
+}());
+

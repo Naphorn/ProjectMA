ca383d0ae99333f39ae74487873ad1072dd12069
Naphorn Chokpokasombat <31033972+Naphorn@users.noreply.github.com>
2019-03-07 21:31:45 +0700

    add charts

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -6,7 +6,32 @@
 @section('content')
 @auth
 
-Home
+<div class="box box-default">
+    <div class="box-header with-border">
+        <h3 class="box-title">Power Status</h3>
+
+        <div class="box-tools pull-right">
+            <button type="button" class="btn btn-box-tool" data-widget="collapse"><i class="fa fa-minus"></i></button>
+            <button type="button" class="btn btn-box-tool" data-widget="remove"><i class="fa fa-times"></i></button>
+        </div>
+    </div>
+    <!-- /.box-header -->
+    <div class="box-body">
+        <div class="row">
+            <div class="col-md-8">
+                <div class="chart-responsive">
+                    <canvas id="pieChart" height="160" width="328" style="width: 328px; height: 160px;"></canvas>
+                </div>
+            </div>
+            <div class="col-md-4">
+                <ul class="chart-legend clearfix">
+                    <li><i class="fa fa-circle-o text-green"></i> On</li>
+                    <li><i class="fa fa-circle-o text-red"></i> Off</li>
+                </ul>
+            </div>
+        </div>
+    </div>
+</div>
 
 @endauth
 @endsection

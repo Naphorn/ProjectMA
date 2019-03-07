c3abadf67d38545b26e2539c844df5459d3bdd13
Naphorn Chokpokasombat <31033972+Naphorn@users.noreply.github.com>
2019-03-08 01:49:48 +0700

    change name

--- resources/views/admin/addlevel.blade.php
+++ resources/views/admin/addlevel.blade.php
@@ -30,7 +30,7 @@
                     <input type="text" name="level_name" class="form-control" placeholder="Level Name" />
                 </div>
                 <div class="form-group">
-                    <input type="text" name="building_id" class="form-control" placeholder="Building Name" />
+                    <input type="text" name="building_id" class="form-control" placeholder="Building ID" />
                 </div>
                 <div class="form-group">
                     <input type="text" name="imglink" class="form-control" placeholder="Link Image" />

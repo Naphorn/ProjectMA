f2204bb9af87e565f10f7913cf4f6cec732b0c9a
Naphorn Chokpokasombat <31033972+Naphorn@users.noreply.github.com>
2019-03-29 03:31:47 +0700

    status detail top page

--- resources/views/building_detail.blade.php
+++ resources/views/building_detail.blade.php
@@ -16,6 +16,21 @@
         </ol>
     </nav>
 
+    <div style="background:#FF0000; width:20px; height:20px; float:left; margin:15px 0px 10px 10px;"></div>
+    <div style="float:left; font-size:20px; margin:10px;">EER is very low</div>
+
+    <div style="background:#FFA500; width:20px; height:20px; float:left; margin:15px 0px 10px 10px;"></div>
+    <div style="float:left; font-size:20px; margin:10px;">EER is low</div>
+
+    <div style="background:#FFFF00; width:20px; height:20px; float:left; margin:15px 0px 10px 10px;"></div>
+    <div style="float:left; font-size:20px; margin:10px;">EER is medium</div>
+
+    <div style="background:#ADFF2F; width:20px; height:20px; float:left; margin:15px 0px 10px 10px;"></div>
+    <div style="float:left; font-size:20px; margin:10px;">EER is good</div>
+
+    <div style="background:#00FF00; width:20px; height:20px; float:left; margin:15px 0px 10px 10px;"></div>
+    <div style="float:left; font-size:20px; margin:10px;">EER is very good</div>
+
     {!! view($levels->imglink)!!}
 
     @foreach($statusairs as $row)
@@ -24,16 +39,16 @@
             if('{{$row->performance}}'==5){
                 cr.style.backgroundColor = "#00FF00"
             }
-            else if('{{$row->performance}}'>3 && '{{$row->performance}}'<=4){
+            else if('{{$row->performance}}'>=4 && '{{$row->performance}}'<=4.99){
                 cr.style.backgroundColor = "#ADFF2F"
             }
-            else if('{{$row->performance}}'>2 && '{{$row->performance}}'<=3){
+            else if('{{$row->performance}}'>=3 && '{{$row->performance}}'<=3.99){
                 cr.style.backgroundColor = "#FFFF00"
             }
-            else if('{{$row->performance}}'>1 && '{{$row->performance}}'<=2){
+            else if('{{$row->performance}}'>=2 && '{{$row->performance}}'<=2.99){
                 cr.style.backgroundColor = "#FFA500"
             }
-            else if('{{$row->performance}}'>=0.00 && '{{$row->performance}}'<=1.00){
+            else if('{{$row->performance}}'>=0.00 && '{{$row->performance}}'<=1.99){
                 cr.style.backgroundColor = "#FF0000"
             }
         </script>

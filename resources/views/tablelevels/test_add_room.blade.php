196c54bef873faee26c9a88cf321dbce95a8b18e
Naphorn Chokpokasombat <31033972+Naphorn@users.noreply.github.com>
2019-03-29 23:48:09 +0700

    test upload flowchar

---
+++ resources/views/tablelevels/test_add_room.blade.php
@@ -0,0 +1,224 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+<meta http-equiv="X-UA-Compatible" content="ie=edge">
+</head>
+<body>
+<style>
+tr
+{mso-height-source:auto;}
+col
+{mso-width-source:auto;}
+br
+{mso-data-placement:same-cell;}
+.style0
+{mso-number-format:General;
+text-align:general;
+vertical-align:bottom;
+white-space:nowrap;
+mso-rotate:0;
+mso-background-source:auto;
+mso-pattern:auto;
+color:black;
+font-size:11.0pt;
+font-weight:400;
+font-style:normal;
+text-decoration:none;
+font-family:Tahoma, sans-serif;
+mso-font-charset:222;
+border:none;
+mso-protection:locked visible;
+mso-style-name:����;
+mso-style-id:0;}
+td
+{mso-style-parent:style0;
+padding-top:1px;
+padding-right:1px;
+padding-left:1px;
+mso-ignore:padding;
+color:black;
+font-size:11.0pt;
+font-weight:400;
+font-style:normal;
+text-decoration:none;
+font-family:Tahoma, sans-serif;
+mso-font-charset:222;
+mso-number-format:General;
+text-align:general;
+vertical-align:bottom;
+border:none;
+mso-background-source:auto;
+mso-pattern:auto;
+mso-protection:locked visible;
+white-space:nowrap;
+mso-rotate:0;}
+.xl65
+{mso-style-parent:style0;
+font-size:18.0pt;
+text-align:center;
+vertical-align:middle;
+border:.5pt solid windowtext;}
+.xl66
+{mso-style-parent:style0;
+font-size:18.0pt;
+vertical-align:middle;}
+</style>
+
+<table border=0 cellpadding=0 cellspacing=0 width=792 style='border-collapse:collapse;table-layout:fixed;width:594pt'>
+<col class=xl66 width=72 span=11 style='width:54pt'>
+<tr height=30 style='height:22.5pt'>
+<td colspan=3 rowspan=4 height=120 class=xl65 width=216 style='height:90.0pt;width:162pt' onclick="ajax_get_data_room_detail('Room 1')" id="Room 1">Room 1</td>
+<td class=xl66 width=72 style='width:54pt'></td>
+<td colspan=3 rowspan=4 class=xl65 width=216 style='width:162pt' onclick="ajax_get_data_room_detail('Room 2')" id="Room 2">Room 2</td>
+<td class=xl66 width=72 style='width:54pt'></td>
+<td colspan=3 rowspan=14 class=xl65 width=216 style='width:162pt' onclick="ajax_get_data_room_detail('Room 5')" id="Room 1">Room 5</td>
+</tr>
+<tr height=30 style='height:22.5pt'>
+<td height=30 class=xl66 style='height:22.5pt'></td>
+<td class=xl66></td>
+</tr>
+<tr height=30 style='height:22.5pt'>
+<td height=30 class=xl66 style='height:22.5pt'></td>
+<td class=xl66></td>
+</tr>
+<tr height=30 style='height:22.5pt'>
+<td height=30 class=xl66 style='height:22.5pt'></td>
+<td class=xl66></td>
+</tr>
+<tr height=30 style='height:22.5pt'>
+<td height=30 class=xl66 style='height:22.5pt'></td>
+<td class=xl66></td>
+<td class=xl66></td>
+<td class=xl66></td>
+<td class=xl66></td>
+<td class=xl66></td>
+<td class=xl66></td>
+<td class=xl66></td>
+</tr>
+<tr height=30 style='height:22.5pt'>
+<td height=30 class=xl66 style='height:22.5pt'></td>
+<td class=xl66></td>
+<td class=xl66></td>
+<td class=xl66></td>
+<td class=xl66></td>
+<td class=xl66></td>
+<td class=xl66></td>
+<td class=xl66></td>
+</tr>
+<tr height=30 style='height:22.5pt'>
+<td height=30 class=xl66 style='height:22.5pt'></td>
+<td class=xl66></td>
+<td colspan=3 rowspan=4 class=xl65 onclick="ajax_get_data_room_detail('Room 3')" id="Room 3">Room 3</td>
+<td class=xl66></td>
+<td class=xl66></td>
+<td class=xl66></td>
+</tr>
+<tr height=30 style='height:22.5pt'>
+<td height=30 class=xl66 style='height:22.5pt'></td>
+<td class=xl66></td>
+<td class=xl66></td>
+<td class=xl66></td>
+<td class=xl66></td>
+</tr>
+<tr height=30 style='height:22.5pt'>
+<td height=30 class=xl66 style='height:22.5pt'></td>
+<td class=xl66></td>
+<td class=xl66></td>
+<td class=xl66></td>
+<td class=xl66></td>
+</tr>
+<tr height=30 style='height:22.5pt'>
+<td height=30 class=xl66 style='height:22.5pt'></td>
+<td class=xl66></td>
+<td class=xl66></td>
+<td class=xl66></td>
+<td class=xl66></td>
+</tr>
+<tr height=30 style='height:22.5pt'>
+<td height=30 class=xl66 style='height:22.5pt'></td>
+<td class=xl66></td>
+<td colspan=3 rowspan=4 class=xl65 onclick="ajax_get_data_room_detail('Room 4')" id="Room 4">Room 4</td>
+<td class=xl66></td>
+<td class=xl66></td>
+<td class=xl66></td>
+</tr>
+<tr height=30 style='height:22.5pt'>
+<td height=30 class=xl66 style='height:22.5pt'></td>
+<td class=xl66></td>
+<td class=xl66></td>
+<td class=xl66></td>
+<td class=xl66></td>
+</tr>
+<tr height=30 style='height:22.5pt'>
+<td height=30 class=xl66 style='height:22.5pt'></td>
+<td class=xl66></td>
+<td class=xl66></td>
+<td class=xl66></td>
+<td class=xl66></td>
+</tr>
+<tr height=30 style='height:22.5pt'>
+<td height=30 class=xl66 style='height:22.5pt'></td>
+<td class=xl66></td>
+<td class=xl66></td>
+<td class=xl66></td>
+<td class=xl66></td>
+</tr>
+<tr height=0 style='display:none'>
+<td width=72 style='width:54pt'></td>
+<td width=72 style='width:54pt'></td>
+<td width=72 style='width:54pt'></td>
+<td width=72 style='width:54pt'></td>
+<td width=72 style='width:54pt'></td>
+<td width=72 style='width:54pt'></td>
+<td width=72 style='width:54pt'></td>
+<td width=72 style='width:54pt'></td>
+<td width=72 style='width:54pt'></td>
+<td width=72 style='width:54pt'></td>
+<td width=72 style='width:54pt'></td>
+</tr>
+</table>
+<!-- Modal -->
+<div class="modal fade" id="exampleModalCenter" tabindex="-1" role="dialog" aria-labelledby="exampleModalCenterTitle" aria-hidden="true">
+<div class="modal-dialog modal-dialog-centered" role="document">
+<div class="modal-content">
+<div class="modal-header">
+<h5 class="modal-title" id="exampleModalCenterTitle">Status</h5>
+<button type="button" class="close" data-dismiss="modal" aria-label="Close">
+<span aria-hidden="true">&times;</span>
+</button>
+</div>
+<div class="modal-body">
+
+</div>
+<div class="modal-footer">
+<button type="button" class="btn btn-secondary" data-dismiss="modal">Close</button>
+</div>
+</div>
+</div>
+</div>
+{{-- ---------------------------------------------------------------------------------------------- --}}
+<!-- Modal -->
+<div class="modal fade" id="modal" role="dialog">
+<!-- <form method="POST" id="form-promote"> -->
+<form class="tagForm" id="tag-form" action="" method="post" enctype="multipart/form-data">
+<div class="modal-dialog">
+<!-- Modal content-->
+<div class="modal-content">
+<div class="modal-header">
+<button type="button" class="close" data-dismiss="modal">&times;</button>
+<h4 class="modal-title"></h4>
+</div>
+<div class="modal-body">
+
+</div>
+<div class="modal-footer">
+<!-- <button type="submit" class="btn btn-primary">Send</button> -->
+<input id="tag-form-submit" type="submit" class="btn btn-primary" value="Send">
+<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
+</div>
+</form>
+</div>
+</body>
+</html>

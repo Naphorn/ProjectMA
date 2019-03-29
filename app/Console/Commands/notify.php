0d5feaea6b760924d9f8dc566567f80346d5d413
Woraprad <31096955+Woraprad@users.noreply.github.com>
2019-03-29 17:34:37 +0700

    สร้าง Commands notify

---
+++ app/Console/Commands/notify.php
@@ -0,0 +1,72 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Illuminate\Console\Command;
+use App\StatusAir;
+class notify extends Command
+{
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'command:name';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Command description';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     *
+     * @return mixed
+     */
+    public function handle()
+    {
+        //
+        $statusairs1 = StatusAir::where('performance','<',3)->pluck('room_no');
+
+        function sendToLine($message){
+        
+
+        $line_api = 'https://notify-api.line.me/api/notify';
+        $line_token = 'ccduc9ATE0AkQrQkFy5mzXHts9PumeniAU6Rlc6Ivoe';
+
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL,"https://notify-api.line.me/api/notify");
+        curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+        curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+        curl_setopt($ch, CURLOPT_POST, 1);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, 'message='.$message);
+        // follow redirects
+        curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
+        curl_setopt($ch, CURLOPT_HTTPHEADER, [
+            'Content-type: application/x-www-form-urlencoded',
+            'Authorization: Bearer '.$line_token,
+        ]);
+        // receive server response ...
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+        $server_output = curl_exec ($ch);
+
+        curl_close ($ch);
+        }
+
+        sendToLine($statusairs1);
+
+        }
+}

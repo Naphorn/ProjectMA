e53b519f1428ebebd82b73ea032015ad21e69a0b
Woraprad <31096955+Woraprad@users.noreply.github.com>
2019-04-14 18:36:02 +0700

    แก้ปัญหาแจ้งเตือนเป็นแบบArray

--- app/Console/Commands/notify.php
+++ app/Console/Commands/notify.php
@@ -38,20 +38,35 @@ class notify extends Command
     public function handle()
     {
         //
-        $statusairs1 = StatusAir::where('performance','<',3)->pluck('room_no');
+        $statusairs1 = StatusAir::where('performance','<',3)->where('level_id',1)
+        ->pluck('room_no')
+        ->pluck('performance');
+        // ->select('room_no','performance')->get();
+        // ->value('room_no','performance');
+        
+        
+        
+        //$detail1 = $statusairs1['room_no'];
+        //$detail1 = json_decode($statusairs1);
+        //$room1 = $detail1->'room_no';
 
+        //$statusairs2 = StatusAir::where('performance','<',3)->where('level_id',2)
+        //->pluck('room_no','performance');
+        //->select('room_no','performance')->get();
         function sendToLine($message){
         
 
         $line_api = 'https://notify-api.line.me/api/notify';
-        $line_token = 'ccduc9ATE0AkQrQkFy5mzXHts9PumeniAU6Rlc6Ivoe';
+        $line_token = 'AGnM9Q9rBjUh8HCLBEa3TdGFrz2Ub6788WUdmpjXew6';
 
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL,"https://notify-api.line.me/api/notify");
         curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
         curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
         curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS, 'message='.$message);
+        curl_setopt($ch, CURLOPT_POSTFIELDS, 'message=Low Performance
+Floor 1
+'.$message);
         // follow redirects
         curl_setopt($ch, CURLOPT_FOLLOWLOCATION, 1);
         curl_setopt($ch, CURLOPT_HTTPHEADER, [
@@ -64,9 +79,42 @@ class notify extends Command
         $server_output = curl_exec ($ch);
 
         curl_close ($ch);
-        }
+        
+        //--------------------------
+//         $ch1 = curl_init();
+//         curl_setopt($ch1, CURLOPT_URL,"https://notify-api.line.me/api/notify");
+//         curl_setopt($ch1, CURLOPT_SSL_VERIFYHOST, 0);
+//         curl_setopt($ch1, CURLOPT_SSL_VERIFYPEER, 0);
+//         curl_setopt($ch1, CURLOPT_POST, 1);
+//         curl_setopt($ch1, CURLOPT_POSTFIELDS, 'message=Low Performance
+// Floor 2
+// '.$message2);
+//         // follow redirects
+//         curl_setopt($ch1, CURLOPT_FOLLOWLOCATION, 1);
+//         curl_setopt($ch1, CURLOPT_HTTPHEADER, [
+//             'Content-type: application/x-www-form-urlencoded',
+//             'Authorization: Bearer '.$line_token,
+//         ]);
+//         // receive server response ...
+//         curl_setopt($ch1, CURLOPT_RETURNTRANSFER, true);
 
-        sendToLine($statusairs1);
+//         $server_output = curl_exec ($ch1);
+
+//         curl_close ($ch);
 
         }
+        // $data1 = implode(" ", (array)$statusairs1);
+        $string = '';
+
+        foreach ($statusairs1 as $key => $value) {
+            $string .= "$value\n";
+        }
+        
+        $string = substr($string, 1); // remove leading ","
+        // foreach($statusairs1 as $row){
+           
+        // }
+        sendToLine($string); 
+
+    }
 }
